1b3b3c91e6502f722038aadaa6411ffe5ad5af9f
Nicholas <nick.nogue28@outlook.com>
2024-11-03 16:52:50 -0300

    add laptop service

---
+++ golang/cmd/client/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"pcbook-golang/pb"
+	"pcbook-golang/sample"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+func main() {
+	serverAddress := flag.String("address", "", "the server address")
+	flag.Parse()
+	log.Printf("dial server %s", *serverAddress)
+
+	conn, err := grpc.NewClient(*serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
+	}
+
+	laptopClient := pb.NewLaptopServiceClient(conn)
+
+	laptop := sample.NewLaptop()
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := laptopClient.CreateLaptop(ctx, req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.AlreadyExists {
+			log.Print("laptop alrealdy exists")
+		} else {
+			log.Fatal("cannot create laptop: ", err)
+		}
+		return
+	}
+
+	log.Printf("created laptop with id: %s", res.Id)
+}
---
+++ golang/service/laptop_client_test.go
@@ -0,0 +1,69 @@
+package service_test
+
+import (
+	"context"
+	"net"
+	"pcbook-golang/pb"
+	"pcbook-golang/sample"
+	"pcbook-golang/serializer"
+	"pcbook-golang/service"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func TestClientCreateLaptop(t *testing.T) {
+	t.Parallel()
+
+	laptopStore := service.NewInMemoryLaptopStore()
+	serverAddress := startTestLaptopServer(t, laptopStore)
+	laptopClient := newTestLaptopClient(t, serverAddress)
+
+	laptop := sample.NewLaptop()
+	expectedID := laptop.Id
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Equal(t, expectedID, res.Id)
+
+	// check that the laptop is saved to the store
+	other, err := laptopStore.Find(res.Id)
+	require.NoError(t, err)
+	require.NotNil(t, other)
+
+	// check that the saved laptop is the same as the one we send
+	requireSameLaptop(t, laptop, other)
+}
+
+func startTestLaptopServer(t *testing.T, laptopStore service.LaptopStore) string {
+	laptopServer := service.NewLaptopServer(laptopStore)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	listener, err := net.Listen("tcp", ":0") // random available port
+	require.NoError(t, err)
+
+	go grpcServer.Serve(listener)
+
+	return listener.Addr().String()
+}
+
+func newTestLaptopClient(t *testing.T, serverAddress string) pb.LaptopServiceClient {
+	conn, err := grpc.NewClient(serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.NoError(t, err)
+	return pb.NewLaptopServiceClient(conn)
+}
+
+func requireSameLaptop(t *testing.T, laptop1 *pb.Laptop, laptop2 *pb.Laptop) {
+	json1 := serializer.ProtobufToJSON(laptop1)
+	json2 := serializer.ProtobufToJSON(laptop2)
+
+	require.Equal(t, json1, json2)
+}
